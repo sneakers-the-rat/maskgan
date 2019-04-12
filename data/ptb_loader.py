de071c589bc270d4372ff0b6e6240a56806d17a2
sneakers-the-rat <JLSaunders987@gmail.com>
2019-04-11 19:19:30 -0700

    adding spaces around eos

--- data/ptb_loader.py
+++ data/ptb_loader.py
@@ -31,7 +31,7 @@ EOS_INDEX = 0
 
 def _read_words(filename):
   with tf.gfile.GFile(filename, "r") as f:
-    return f.read().decode("utf-8").replace("\n", "<eos>").split()
+    return f.read().decode("utf-8").replace("\n", " <eos> ").split()
 
 
 def build_vocab(filename):
